ddfcd9e3f1edcd101dbbb18bfab287f3357ec983
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-04-01 18:46:28 +0400

    update: class-component-childer-error-boundaury

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
+import { useState } from "react";
+
+import Theme from "./components/theme";
+import Counter from "./pages/counter/counter";
 import TodoHome from "./pages/todo/todo-home";
+import Navigation from "./components/navigation/navigation";
 // import Home from "./pages/home/home";
 // import Welcome from "./components/welcome";
 
 import "./App.css";
 
 function App() {
+  const [page, setPage] = useState({
+    todoHome: false,
+    counterHome: true,
+  });
+  const [currentPage, setCurrentPage] = useState("counterHome");
+
+  const onPageChange = (pageKey) => {
+    const updateActivePage = { ...page };
+    let newCurrentPage = "";
+    Object.keys(updateActivePage).forEach((key) => {
+      if (key === pageKey) {
+        updateActivePage[pageKey] = true;
+        newCurrentPage = pageKey;
+      } else {
+        updateActivePage[key] = false;
+      }
+    });
+
+    setCurrentPage(newCurrentPage);
+    setPage(updateActivePage);
+  };
+
   return (
     <div className="container">
-      <TodoHome />
+      <Navigation onPageChange={onPageChange} pages={page} />
+      <Theme page={currentPage}>
+        {page.todoHome && <TodoHome />}
+        {page.counterHome && <Counter />}
+      </Theme>
     </div>
   );
 }
